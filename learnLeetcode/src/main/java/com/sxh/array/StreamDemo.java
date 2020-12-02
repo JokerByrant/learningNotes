9e8f42b2710d1115d3899fb5e82ac63cc46ed6b4
sxhjoker <sxhjoker@163.com>
2020-12-02 17:02:20 +0800

    基础类型、包装类型数组求和

--- learnLeetcode/src/main/java/com/sxh/array/StreamDemo.java
+++ learnLeetcode/src/main/java/com/sxh/array/StreamDemo.java
@@ -2,7 +2,10 @@ package com.sxh.array;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -29,14 +32,10 @@ public class StreamDemo {
         // 方式4
         Integer sum4 = set.stream().collect(Collectors.summingInt(x -> x));
 
-        // 数组求和
-        int sumArr = Arrays.stream(set.toArray()).mapToInt(x -> (int) x).sum();
-
         System.out.println("方式1--->mapToInt求和结果：" + sum1);
         System.out.println("方式2--->reduce求和结果：" + sum2);
         System.out.println("方式3--->reduce求和结果：" + sum3);
         System.out.println("方式4--->collect求和结果：" + sum4);
-        System.out.println("数组求和：" + sumArr);
     }
 
     // 2.基础类型数组转换成list、Set
@@ -49,5 +48,25 @@ public class StreamDemo {
         // 注：下面几种方法返回的都是Set<int[]>类型的集合
         Set<int[]> collect = Stream.of(a).collect(Collectors.toSet());
         Set<int[]> set = new HashSet<>(Arrays.asList(a));
+    }
+
+    // 3.基础类型数组、包装类型数组求和
+    @Test
+    public void fun3() {
+        // 基础类型数组求和
+        int[] a = {1,2,3,4,5,6};
+        int sumA = Arrays.stream(a).sum();
+
+        // 包装类型数组求和，主要就是利用Arrays.stream()将数组放在流里进行操作
+        Integer[] b = {1,2,3,4,5,6};
+        // 1.mapToInt把Stream转换成了InputStream，然后执行的sum()操作与上面基础类型sum()操作一致
+        int sumB1 = Arrays.stream(b).mapToInt(x->x).sum();
+        int sumB2 = Arrays.stream(b).reduce((x, y)->x+y).get();
+        int sumB3 = Arrays.stream(b).reduce(0, (x, y) -> x + y);
+
+        System.out.println("基础类型求和：" + sumA);
+        System.out.println("包装类型求和1：" + sumB1);
+        System.out.println("包装类型求和2：" + sumB2);
+        System.out.println("包装类型求和3：" + sumB3);
     }
 }
